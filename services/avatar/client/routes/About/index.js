0dd142d5f91baf611d41e6e92730814fd7b45ac2
bandeyaatif <aatif.bandey@tokopedia.com>
2021-05-03 02:02:49 +0530

    loadable component set up

---
+++ services/avatar/client/routes/About/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const About = () => {
+  return  (
+    <div> About us page.... under construction</div>
+  );
+  
+}
+
+export default About;
--- services/avatar/client/routes/index.js
+++ services/avatar/client/routes/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import loadable from '@loadable/component'
 
-import Home from "./Home";
+
+const Home = loadable(
+  () => import(/* webpackChunkName: "home" */ '@routes/Home/'),
+  {
+    fallback: '<div> Loading chunk ....</div>',
+    ssr: true,
+  },
+);
+
+const About = loadable(
+  () => import(/* webpackChunkName: "about" */ '@routes/About/'),
+  {
+    fallback: '<div> Loading chunk ....</div>',
+    ssr: true,
+  },
+);
+
+
 import Layout from "../components/Layout";
 
 const Routes = () => {
@@ -12,6 +30,9 @@ const Routes = () => {
         <Switch>
           <Route path="/" exact>
             <Home />
+          </Route>
+          <Route path="/about-us" exact>
+            <About />
           </Route>
         </Switch>
       </>
